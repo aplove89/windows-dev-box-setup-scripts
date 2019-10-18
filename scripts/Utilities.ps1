238b5783c11fc15f2329b52bc1435f1bd91ff083
APLove <aplove89@gmail.com>
2019-10-18 13:15:37 -0500

    fix copy pasta mistake

--- scripts/Utilities.ps1
+++ scripts/Utilities.ps1
@@ -1,18 +1,18 @@
 
 # Utilities
 choco install -y sudo
-choco install vscode -y sysinternals
-choco install vscode -y windirstat
-choco install vscode -y winmerge
-choco install vscode -y winscp.portable
-choco install vscode -y dngrep
-choco install vscode -y notepadplusplus.install --x86
-choco install vscode -y mremoteng
-choco install vscode -y postman
-choco install vscode -y fiddler
-choco install vscode -y pandoc
-choco install vscode -y filezilla
-choco install vscode -y conemu
-choco install vscode -y docker-for-windows
-choco install vscode -y 7zip.install
-choco install vscode -y procexp
+choco install -y sysinternals
+choco install -y windirstat
+choco install -y winmerge
+choco install -y winscp.portable
+choco install -y dngrep
+choco install -y notepadplusplus.install --x86
+choco install -y mremoteng
+choco install -y postman
+choco install -y fiddler
+choco install -y pandoc
+choco install -y filezilla
+choco install -y conemu
+choco install -y docker-for-windows
+choco install -y 7zip.install
+choco install -y procexp
