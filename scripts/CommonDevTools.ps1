a899c4a93cfe2fe5995afcae2be824ade0c4cb45
APLove <aplove89@gmail.com>
2019-10-18 12:56:04 -0500

    updated dev tools to my liking

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,7 +1,7 @@
 
 # tools we expect devs across many scenarios will want
-choco install -y vscode
-choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
-choco install -y python
-choco install -y 7zip.install
-choco install -y sysinternals
+choco install -y vscode --params "/NoDesktopIcon"
+choco install -y git.install --params="'/GitAndUnixToolsOnPath /WindowsTerminal /NoGuiHereIntegration'"
+choco install -y github-desktop
+choco install -y cs-script
+choco install -y sql-server-management-studio
