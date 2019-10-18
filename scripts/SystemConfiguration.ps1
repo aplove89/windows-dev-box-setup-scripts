e5731a9f6c1aae43f5895ed92c773bcb71a34343
APLove <aplove89@gmail.com>
2019-10-18 13:09:58 -0500

    enable windowsOptionalFeatures

--- scripts/SystemConfiguration.ps1
+++ scripts/SystemConfiguration.ps1
@@ -2,3 +2,11 @@
 
 #--- Enable developer mode on the system ---
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+
+# Enable Windows Features...
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
+Enable-WindowsOptionalFeature -Online -FeatureName HypervisorPlatform -NoRestart
+RefreshEnv
