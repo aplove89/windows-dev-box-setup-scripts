3e4bacc331e51904c24792190dca50a6e4775c88
APLove <aplove89@gmail.com>
2019-10-18 12:58:22 -0500

    added vscode stuff

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,6 +1,11 @@
 
 # tools we expect devs across many scenarios will want
 choco install -y vscode --params "/NoDesktopIcon"
+choco install -y vscode-powershell
+choco install -y vscode-gitlens
+choco install -y vscode-editorconfig
+choco install -y vscode-csharp
+choco install -y vscode-docker
 choco install -y git.install --params="'/GitAndUnixToolsOnPath /WindowsTerminal /NoGuiHereIntegration'"
 choco install -y github-desktop
 choco install -y cs-script
