ec80e4bf65f3ddac64a30afd0ef9162ac7bf208d
APLove <aplove89@gmail.com>
2019-10-18 13:12:54 -0500

    my favorite utilities

---
+++ scripts/Utilities.ps1
@@ -0,0 +1,18 @@
+
+# Utilities
+choco install -y sudo
+choco install vscode -y sysinternals
+choco install vscode -y windirstat
+choco install vscode -y winmerge
+choco install vscode -y winscp.portable
+choco install vscode -y dngrep
+choco install vscode -y notepadplusplus.install --x86
+choco install vscode -y mremoteng
+choco install vscode -y postman
+choco install vscode -y fiddler
+choco install vscode -y pandoc
+choco install vscode -y filezilla
+choco install vscode -y conemu
+choco install vscode -y docker-for-windows
+choco install vscode -y 7zip.install
+choco install vscode -y procexp
